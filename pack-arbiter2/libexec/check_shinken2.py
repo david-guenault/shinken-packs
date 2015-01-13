900b7a0c1522bf0a926ded3834829b53ea7737fd
David GUENAULT <david.guenault@gmail.com>
2015-01-13 08:11:16 +0100

    Added timeout support for request, added a detailled output for every daemon with -v option, added multiarbiter support, remove useless thresholds

--- pack-arbiter2/libexec/check_shinken2.py
+++ pack-arbiter2/libexec/check_shinken2.py
@@ -31,10 +31,12 @@ UNKNOWN = 3
 
 daemon_types = ['arbiter', 'broker', 'scheduler', 'poller', 'reactionner']
 
-def ping(host=None, port=None, proto="http"):
+
+
+def ping(host=None, port=None, proto="http", timeout=1):
     uri = "%s://%s:%s/ping" % (proto, host, port)
     try:
-        result = requests.get(uri)
+        result = requests.get(uri, timeout=timeout)
         if result.text == "\"pong\"":
             message = { "message":"pong", "status":True }
         else:
@@ -46,10 +48,10 @@ def ping(host=None, port=None, proto="http"):
 
     return message    
 
-def get_status(host=None, port=None, proto="http", target=None, daemon=None):
+def get_status(host=None, port=None, proto="http", target=None, daemon=None, timeout=1):
     uri = "%s://%s:%s/get-all-states" % (proto, host, port)
     try:
-        result = requests.get(uri).json()
+        result = requests.get(uri, timeout=timeout).json()
     except requests.exceptions.ConnectionError:
         message = { "message":"Connection error", "status":False }
     except requests.exceptions.Timeout:
@@ -80,40 +82,59 @@ def get_status(host=None, port=None, proto="http", target=None, daemon=None):
 
     return message
  
-def get_all_status(host=None, port=None, proto="http"):
+def get_all_status(host=None, port=None, proto="http", timeout=1):
     uri = "%s://%s:%s/get-all-states" % (proto, host, port)
     try:
-        result = requests.get(uri).json()
+        result = requests.get(uri, timeout=timeout).json()
     except requests.exceptions.ConnectionError:
         message = { "message":"Connection error", "status":False }
     except requests.exceptions.Timeout:
         message = { "message":"Timeout", "status":False }
     finally:
-        print "+%s+" % (85*"-")
-        print "| {:^20} | {:^20} | {:^15} | {:^19} |".format("TYPE","NAME","STATUS","REALM")
-        print "+%s+" % (85*"-")
+
+        print "From :", host
+        print
+
+        print "+%s+" % (106*"-")
+        print "| {:^20} | {:^20} | {:^15} | {:^19} | {:^8} | {:^7} |".format("TYPE","NAME","STATUS","REALM","ATTEMPTS","SPARE")
+        print "+%s+" % (106*"-")
         for key,data in result.iteritems():
             for daemon in data:
+
+                attempts = "%s/%s" % (daemon["attempt"],daemon["max_check_attempts"])
+
                 if not daemon["alive"]:
                     alive= "dead"
                 else:
-                    alive= "alive"
-                print "| {:20} | {:20} | {:^15} | {:19} |".format(key,daemon["%s_name" % key], alive, daemon["realm"])
-        print "+%s+" % (85*"-")
+                    if daemon["attempt"] > 0:
+                        alive = "retry"
+                    else:
+                        alive= "alive"
+
+                if daemon["spare"]:
+                    spare = "X"
+                else:
+                    spare = " "
+
+                if "realm" in daemon.keys():
+                    realm = daemon["realm"]
+                else:
+                    realm = ""
+
+                print "| {:20} | {:20} | {:^15} | {:19} | {:^8} | {:^7} |".format(key,daemon["%s_name" % key], alive, realm, attempts, spare)
+        print "+%s+" % (106*"-")
 
 
 
 if __name__ == '__main__':
 
     parser = OptionParser()
-    parser.add_option('-a', '--hostname', dest='hostname', default='127.0.0.1')
+    parser.add_option('-a', '--hostnames', dest='hostnames', default='127.0.0.1')
     parser.add_option('-p', '--portnumber', dest='portnum', default=7770, type=int)
     parser.add_option('-s', '--ssl', action="store_true", dest='ssl', default=False)
     parser.add_option('-t', '--target', dest='target',default=False,type=str)
     parser.add_option('-d', '--daemonname', dest='daemon', default='')
-    # parser.add_option('-w', '--warning', dest='warning', default=1, type=int)
-    # parser.add_option('-c', '--critical', dest='critical', default=0, type=int)
-    parser.add_option('-T', '--timeout', dest='timeout', default=10, type=float)
+    parser.add_option('-T', '--timeout', dest='timeout', default=1, type=float)
     parser.add_option('-v', '--verbose', action="store_true", dest='verbose', default=False)
 
 
@@ -126,16 +147,28 @@ if __name__ == '__main__':
     else:
         proto = "http"
 
-    # first we ping the arbiter
-    result = ping(host=options.hostname, port=options.portnum, proto=proto)
-    if not result["status"]:
-        print "CRITICAL : the Arbiter is not reachable : (%s)." % result["message"] 
-        raise sys.exit(CRITICAL)
+    # first we ping the arbiters unless we find one alive
+    if "," in options.hostnames:
+        hostnames = options.hostnames.split(",")
+    else:
+        hostnames = [ options.hostnames ]
+
+    hostname = False
+
+    for h in hostnames:
+        result = ping(host=h, port=options.portnum, proto=proto, timeout=options.timeout)
+        if result["status"]:
+            hostname = h
+            break
+
+    if not hostname:
+        # no arbiter are alive !
+        print "CRITICAL : No arbiter reachable !" 
+        sys.exit(CRITICAL)
 
     # detailled output and no more
     if options.verbose:
-        get_all_status(host=options.hostname, port=options.portnum, proto=proto)
-        raise sys.exit(OK)
+        get_all_status(host=hostname, port=options.portnum, proto=proto, timeout=options.timeout)
         sys.exit(OK)
 
     # Check for required option target
@@ -143,16 +176,16 @@ if __name__ == '__main__':
         print ('CRITICAL - target is not specified; '
                'You must specify which daemon type you want to check!')
         parser.print_help()
-        raise sys.exit(CRITICAL)
+        sys.exit(CRITICAL)
     elif options.target not in daemon_types:
         print 'CRITICAL - target', options.target, 'is not a Shinken daemon!'
         parser.print_help()
-        raise sys.exit(CRITICAL)
+        sys.exit(CRITICAL)
 
 
 
     # get daemons status (target = daemon type, daemon = daemon name)
-    result = get_status(host=options.hostname, port=options.portnum, proto=proto, target=options.target, daemon = options.daemon)
+    result = get_status(host=hostname, port=options.portnum, proto=proto, target=options.target, daemon = options.daemon, timeout=options.timeout)
     if not "status" in result.keys() or not result["status"]:
         print "Error : ", result["message"]
         sys.exit(UNKNOWN)
